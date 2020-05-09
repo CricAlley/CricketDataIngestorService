61c75d2b762e7fb7a4b32c47e4e8117dfa06f614
narender <naru1790@gmail.com>
2020-05-09 18:07:39 +0530

    wicket count

--- CricketDataIngester/YamlParser/Wicket.cs
+++ CricketDataIngester/YamlParser/Wicket.cs
@@ -5,6 +5,8 @@ namespace CricketDataIngester.YamlParser
 {
     public class Wicket
     {
+        public int Count => 1;
+
         [YamlMember(Alias = "player_out", ApplyNamingConventions = false)]
         public string PlayerOut { get; set; }
         public List<string> Fielders { get; set; }
